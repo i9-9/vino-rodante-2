53acae679d50e6d142b3e9de77444ab5b197ca7d
i9-9 <yvan.vrs@gmail.com>
2025-10-30 14:15:05 -0300

    currency: show ARS code in UI prices

--- lib/utils.ts
+++ lib/utils.ts
@@ -10,6 +10,7 @@ export function formatCurrency(amount: number): string {
   return new Intl.NumberFormat("es-AR", {
     style: "currency",
     currency: "ARS",
+    currencyDisplay: "code",
   }).format(amount)
 }
 
@@ -25,6 +26,7 @@ export function formatPrice(price: number): string {
   return new Intl.NumberFormat('es-AR', {
     style: 'currency',
     currency: 'ARS',
+    currencyDisplay: 'code',
     minimumFractionDigits: 0,
     maximumFractionDigits: 0
   }).format(price)
